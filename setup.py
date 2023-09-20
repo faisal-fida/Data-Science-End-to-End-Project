762574efaf5c0628c796424b4ada32a6e199dd6a
faisal-fida <arainfaisal826@gmail.com>
2023-09-20 20:55:29 +0500

    First Commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+HYPHEN_E_DOT = '-e .'
+
+def find_libraries(requirement_file):
+    
+    requirements = []
+
+    with open(requirement_file) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [i.replace('\n','') for i in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name='mlproject',
+    version='1.0',
+    description="Machine Learning Project",
+    author="Dev Faisal",
+    packages=find_packages(),
+    install_requires=find_libraries("requirements.txt"),
+)
